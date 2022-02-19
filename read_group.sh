5fb4d279522c802bee1b03064462fb2107dbfbc9
Tram <vbt576@gmail.com>
2022-02-19 17:34:11 +0700

    new header format

--- read_group.sh
+++ read_group.sh
@@ -11,7 +11,14 @@ else
     header=$(cat $1 | head -n 1)
 fi
 
-id=$(echo $header | cut -f1 -d"L" | sed 's/@//')
-sm=$(basename $(dirname $1))
-l=$(echo $header | cut -f2 -d"L" | cut -f1 -d"C")
-echo "@RG\tID:$id.$l\tSM:$sm\tPL:Illumina"
+if [[ -z $(echo $header | grep : )]]
+then
+    id=$(echo $header | cut -f1 -d"L" | sed 's/@//')
+    sm=$(basename $(dirname $1))
+    l=$(echo $header | cut -f2 -d"L" | cut -f1 -d"C")
+    echo "@RG\tID:$id.$l\tSM:$sm\tPL:Illumina"
+else
+    id=$(echo $header | cut -f1 -d":" | sed 's/@//')
+    sm=$(basename $(dirname $1))
+    l=$(echo $header | cut -f2 -d":")
+    echo "@RG\tID:$id.$l\tSM:$sm\tPL:Illumina"
